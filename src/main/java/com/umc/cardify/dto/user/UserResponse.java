97aa99eca80f90b597bff6cd880b9533bbd7cbff
dhchoi98 <chleogks9808@naver.com>
2024-07-18 11:21:03 +0900

    [Feat] #4 : UserController, DTO에 스웨거 어노테이션 적용

--- src/main/java/com/umc/cardify/dto/user/UserResponse.java
+++ src/main/java/com/umc/cardify/dto/user/UserResponse.java
@@ -1,20 +1,23 @@
 package com.umc.cardify.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 public class UserResponse {
 
-    @Getter
-    @Builder
-    //@Schema(title = "USER_RES_01 : 로그인 응답 DTO (토큰)")
-    public static class tokenInfo {
-        //@Schema(description = "토큰의 타입")
-        private String grantType;
-        //@Schema(description = "액세스 토큰")
-        private String accessToken;
-        //@Schema(description = "리프레시 토큰")
-        private String refreshToken;
-    }
+	@Getter
+	@Builder
+	@Schema(title = "USER_RES_01 : 로그인 응답 DTO (토큰)")
+	public static class tokenInfo {
+		@Schema(description = "토큰의 타입")
+		private String grantType;
+
+		@Schema(description = "액세스 토큰")
+		private String accessToken;
+
+		@Schema(description = "리프레시 토큰")
+		private String refreshToken;
+	}
 
 }
