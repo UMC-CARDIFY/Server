fbc9837f86175f7829b81a566bb2b21d17ff0f71
dhchoi98 <chleogks9808@naver.com>
2024-07-21 19:06:11 +0900

    [FEAT] #12 : S3 버킷에 파일 업로드 및 삭제를 위한 S3Service 추가

---
+++ src/main/java/com/umc/cardify/service/S3Service.java
@@ -0,0 +1,83 @@
+package com.umc.cardify.service;
+
+import static com.umc.cardify.config.exception.ErrorResponseStatus.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.umc.cardify.config.exception.AwsS3Exception;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+	private final AmazonS3 s3Client;
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucket;
+
+	// 여러개의 파일 업로드
+	public List<String> uploadFile(List<MultipartFile> multipartFile) {
+		List<String> fileNameList = new ArrayList<>();
+
+		multipartFile.forEach(file -> {
+			String fileName = createFileName(file.getOriginalFilename());
+			ObjectMetadata objectMetadata = new ObjectMetadata();
+			objectMetadata.setContentLength(file.getSize());
+			objectMetadata.setContentType(file.getContentType());
+
+			try (InputStream inputStream = file.getInputStream()) {
+				s3Client.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata).withCannedAcl(
+					CannedAccessControlList.PublicRead));
+			} catch (IOException e) {
+				throw new AwsS3Exception(IMAGE_UPLOAD_ERROR);
+			}
+
+			fileNameList.add(fileName);
+		});
+
+		return fileNameList;
+	}
+
+	// 파일 삭제
+	public void deleteFile(String fileName) {
+		s3Client.deleteObject(new DeleteObjectRequest(bucket, fileName));
+	}
+
+	// 파일명 중복 방지 (UUID)
+	private String createFileName(String fileName) {
+		return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+	}
+
+	// 파일 유효성 검사
+	private String getFileExtension(String fileName) {
+		if (fileName.isEmpty()) {
+			throw new AwsS3Exception(FILE_VALID_ERROR);
+		}
+		ArrayList<String> fileValidate = new ArrayList<>();
+		fileValidate.add(".jpg");
+		fileValidate.add(".jpeg");
+		fileValidate.add(".png");
+		fileValidate.add(".JPG");
+		fileValidate.add(".JPEG");
+		fileValidate.add(".PNG");
+		String idxFileName = fileName.substring(fileName.lastIndexOf("."));
+		if (!fileValidate.contains(idxFileName)) {
+			throw new AwsS3Exception(FILE_FORMAT_ERROR);
+		}
+		return fileName.substring(fileName.lastIndexOf("."));
+	}
+}
