f521b43a1236b7eed156e950152f6c47171e60bf
dhchoi98 <chleogks9808@naver.com>
2024-07-24 10:51:20 +0900

    [FEAT] #12 : 이미지(가림판) 카드 추가 API 구현 (Controller, Service)

---
+++ src/main/java/com/umc/cardify/service/CardService.java
@@ -0,0 +1,54 @@
+package com.umc.cardify.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.umc.cardify.domain.ImageCard;
+import com.umc.cardify.domain.Overlay;
+import com.umc.cardify.dto.card.CardRequest;
+import com.umc.cardify.repository.ImageCardRepository;
+import com.umc.cardify.repository.OverlayRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CardService {
+
+	private final S3Service s3Service;
+
+	private final ImageCardRepository imageCardRepository;
+
+	private final OverlayRepository overlayRepository;
+
+	public String addImageCard(Long userId, MultipartFile image, CardRequest.addImageCard request) {
+		String imgUrl = s3Service.upload(image, "imageCards");
+
+		ImageCard imageCard = ImageCard.builder()
+			.imageUrl(imgUrl)
+			.height(request.getBaseImageHeight())
+			.width(request.getBaseImageWidth())
+			.build();
+
+		ImageCard savedImageCard = imageCardRepository.save(imageCard);
+
+		if (request.getOverlays() != null) {
+			for (CardRequest.addImageCardOverlay overlayRequest : request.getOverlays()) {
+				Overlay overlay = Overlay.builder()
+					.xPosition(overlayRequest.getPositionOfX())
+					.yPosition(overlayRequest.getPositionOfY())
+					.width(overlayRequest.getWidth())
+					.height(overlayRequest.getHeight())
+					.imageCard(savedImageCard)
+					.build();
+
+				// Overlay를 데이터베이스에 저장
+				overlayRepository.save(overlay);
+			}
+		}
+
+		return savedImageCard.getImageUrl();
+	}
+}
