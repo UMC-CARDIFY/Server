afe790d2523f568c3974b42db8c1eea051781b51
zwei1garden <dlwjddnjs081723@naver.com>
2024-08-02 10:14:28 +0900

    [FEAT]#57 : 스웨거 CORS 에러 및 403 에러 해결

--- src/main/java/com/umc/cardify/config/WebConfig.java
+++ src/main/java/com/umc/cardify/config/WebConfig.java
@@ -1,38 +1,34 @@
 package com.umc.cardify.config;
 
-import com.umc.cardify.jwt.JwtInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import lombok.RequiredArgsConstructor;
-
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
-    private final JwtInterceptor jwtInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/v1/folders/**");// Apply to specific patterns
-        registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/v1/notes/**");
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080/**", "http://3.37.13.40:8080/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true);
+        // JwtInterceptor 제거
     }
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-}
 
+    // CORS 설정을 사용하는 경우 주석 해제
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("") // 특정 출처로 제한할 수 있음
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
