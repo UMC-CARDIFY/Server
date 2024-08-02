992e6479ec7090bcdf592d13acd91ef82d89c250
SooD <dlatnqls921@gmail.com>
2024-08-02 21:06:15 +0900

    [Feat] #58 : 즐겨찾기, 정렬 기능 수정

--- src/main/java/com/umc/cardify/service/FolderService.java
+++ src/main/java/com/umc/cardify/service/FolderService.java
@@ -15,10 +15,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -73,18 +73,26 @@ public class FolderService {
                 .build();
     }
 
-    public FolderResponse.sortFolderListDTO sortFoldersByUserId(Long userId, int page, int size, String order){
+    public FolderResponse.sortFolderListDTO sortFoldersByUserId(Long userId, Integer page, Integer size, String order){
         User user = userRepository.findById(userId)
-                .orElseThrow(()-> new IllegalArgumentException("User not found with id: " + userId));
-        Pageable pageable = switch (order.toLowerCase()) {
-            case "asc" -> PageRequest.of(page, size, Sort.by("name").ascending());
-            case "desc" -> PageRequest.of(page, size, Sort.by("name").descending());
-            case "edit-newest" -> PageRequest.of(page, size, Sort.by("editDate").ascending());
-            case "edit-oldest" -> PageRequest.of(page, size, Sort.by("editDate").descending());
-            default -> throw new BadRequestException(ErrorResponseStatus.REQUEST_ERROR);
-        };
+                .orElseThrow(()-> new BadRequestException(ErrorResponseStatus.INVALID_USERID));
 
-        Page<Folder> folderPage = folderRepository.findByUser(user, pageable);
+        int sortFolderPage = (page!=null) ? page:0;
+        int sortFolderSize = (size!=null) ? size:30;
+
+        switch(order){
+            case "asc":
+            case "desc":
+            case "edit-newest":
+            case "edit-oldest":
+                break;
+            default:
+                throw new BadRequestException(ErrorResponseStatus.REQUEST_ERROR);
+        }
+
+        Pageable pageable = PageRequest.of(sortFolderPage, sortFolderSize);
+        Page<Folder> folderPage = folderRepository.findByUserAndSort(user, order, pageable);
+
         List<FolderResponse.sortFolderInfoDTO> folders = folderPage.getContent().stream()
                 .map(folder -> FolderResponse.sortFolderInfoDTO.builder()
                         .folderId(folder.getFolderId())
