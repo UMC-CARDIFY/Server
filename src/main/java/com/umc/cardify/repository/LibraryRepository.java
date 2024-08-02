1c6dc6319f5128bf443afb7db4b04c50b0f8d0d7
ohige01 <ohige01@naver.com>
2024-08-03 00:39:21 +0900

    [FEAT]#55: 노트 자료실 업로드 버그 수정 및 유효성 검사

--- src/main/java/com/umc/cardify/repository/LibraryRepository.java
+++ src/main/java/com/umc/cardify/repository/LibraryRepository.java
@@ -1,7 +1,9 @@
 package com.umc.cardify.repository;
 
 import com.umc.cardify.domain.Library;
+import com.umc.cardify.domain.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LibraryRepository extends JpaRepository<Library, Long> {
+    Library findByNote(Note note);
 }
