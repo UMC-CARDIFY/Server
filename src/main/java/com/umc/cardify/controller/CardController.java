06b114f5f852a97d4f52f968944f0b7196ae2c3f
dhchoi98 <chleogks9808@naver.com>
2024-08-10 18:13:20 +0900

    [FEAT] #93 : 이미지 카드 조회 API 구현

--- src/main/java/com/umc/cardify/controller/CardController.java
+++ src/main/java/com/umc/cardify/controller/CardController.java
@@ -1,9 +1,9 @@
 package com.umc.cardify.controller;
 
-import java.util.List;
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +11,10 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.umc.cardify.config.exception.AwsS3Exception;
-import com.umc.cardify.config.exception.ErrorResponseStatus;
 import com.umc.cardify.dto.card.CardRequest;
+import com.umc.cardify.dto.card.CardResponse;
 import com.umc.cardify.jwt.JwtUtil;
 import com.umc.cardify.service.CardService;
-import com.umc.cardify.service.S3Service;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -35,12 +33,20 @@ public class CardController {
 	@PostMapping(value = "/add/Image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	@Operation(summary = "이미지 카드 생성", description = "이미지 및 가림판들의 크기와 위치 전송")
 	public ResponseEntity<String> addImageCard(@RequestHeader("Authorization") String token,
-		@RequestPart("image") MultipartFile image, @RequestPart("imageCard")CardRequest.addImageCard request){
-		Long userId = jwtUtil.extractUserId(token);
+		@RequestPart("image") MultipartFile image, @RequestPart("imageCard") CardRequest.addImageCard request) {
+		//Long userId = jwtUtil.extractUserId(token);
+		Long userId = 1L;
 
 		String imgUrl = cardService.addImageCard(userId, image, request);
 
 		return ResponseEntity.ok(imgUrl);
+	}
+
+	@GetMapping(value = "/view/{imgCardId}/Image")
+	@Operation(summary = "이미지 카드 조회", description = "이미지 및 가림판들의 크기와 위치 조회")
+	public ResponseEntity<CardResponse.getImageCard> viewImageCard(@PathVariable Long imgCardId) {
+
+		return ResponseEntity.ok(cardService.viewImageCard(imgCardId));
 	}
 
 }
