e528824719bea3d304cad2ede880fe0966535946
ohige01 <ohige01@naver.com>
2024-08-11 01:11:32 +0900

    [REFACTOR]: 자료실 특정 카테고리 내 노트조회 API 정렬 방식 설정

--- src/main/java/com/umc/cardify/dto/library/LibraryResponse.java
+++ src/main/java/com/umc/cardify/dto/library/LibraryResponse.java
@@ -45,6 +45,7 @@ public class LibraryResponse {
         String userName;
         Integer cntDownloadAll;
         Integer cntDownloadWeek;
+        LocalDateTime uploadAt;
     }
     @Getter
     @Builder
