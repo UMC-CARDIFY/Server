50aa5813309a97a4d7852a24f4fb9ffd015b5fd4
dhchoi98 <chleogks9808@naver.com>
2024-08-21 22:39:07 +0900

    [FEAT] #171 : 출석 체크 API 구현

--- src/main/java/com/umc/cardify/repository/UserRepository.java
+++ src/main/java/com/umc/cardify/repository/UserRepository.java
@@ -1,19 +1,27 @@
 package com.umc.cardify.repository;
 
-import com.umc.cardify.domain.User;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.umc.cardify.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUserId(Long userId);
-    boolean existsByEmail(String email); // 중복 검사 메서드
+	Optional<User> findByEmail(String email);
 
-    Optional<User> findByEmailAndKakao(String email, boolean kakao);
+	Optional<User> findByUserId(Long userId);
 
+	boolean existsByEmail(String email); // 중복 검사 메서드
+
+	Optional<User> findByEmailAndKakao(String email, boolean kakao);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE User u SET u.todayCheck = 0")
+	void resetAllTodayCheck();
 }
