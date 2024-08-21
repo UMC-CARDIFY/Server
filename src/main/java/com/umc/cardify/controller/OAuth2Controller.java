dae52b68acc8d1aa2ffa5e778aec473af01245f3
Jeong Won Lee <106802869+zwei1garden@users.noreply.github.com>
2024-08-21 20:42:24 +0900

    [FIX] https 카카오 로그인을 위한 설정

--- src/main/java/com/umc/cardify/controller/OAuth2Controller.java
+++ src/main/java/com/umc/cardify/controller/OAuth2Controller.java
@@ -11,19 +11,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.util.UriComponentsBuilder;
+
 import java.util.Collections;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 @Tag(name = "OAuth2Controller", description = "카카오 로그인 관련 API")
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1/oauth2")
 @RequiredArgsConstructor
 public class OAuth2Controller {
 
     private final KakaoService kakaoService;
-    private static final Logger log = LoggerFactory.getLogger(OAuth2Controller.class);
 
     @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
     private String clientId;
@@ -35,30 +34,36 @@ public class OAuth2Controller {
     private String redirectUri;
 
     // 인증 관련
-    @GetMapping("/api/v1/oauth2/authorization/kakao")
+    @GetMapping("/authorization/kakao")
     public ResponseEntity<Map<String, String>> getKakaoAuthorizationUrl() {
         String kakaoAuthUrl = UriComponentsBuilder
                 .fromUriString("https://kauth.kakao.com/oauth/authorize")
                 .queryParam("client_id", clientId)
+                .queryParam("client-secret", clientSecret)
                 .queryParam("redirect_uri", redirectUri)
                 .queryParam("response_type", "code")
                 .toUriString();
+
         return ResponseEntity.ok(Collections.singletonMap("authorizationUrl", kakaoAuthUrl));
     }
 
     // 콜백
-    @GetMapping("oauth2/callback/kakao")
-    public ResponseEntity<UserResponse.tokenInfo> kakaoCallback(@RequestParam String code, HttpServletRequest request) throws JsonProcessingException {
-        log.info("Received kakao callback request. Request ID: {}", request.getSession().getId());
+    @PostMapping("callback/kakao")
+    public ResponseEntity<UserResponse.tokenInfo> kakaoCallback(@RequestBody Map<String, String> payload, HttpServletRequest request) throws JsonProcessingException {
+        System.out.println("Received kakao callback request. Request ID: " + request.getSession().getId());
         try {
-            log.info("Processing login for code: {}", code);
+            String code = payload.get("code");
+            if (code == null || code.isEmpty()) {
+                throw new IllegalArgumentException("Code is missing or empty");
+            }
+            System.out.println("Processing login for code: " + code);
             UserResponse.tokenInfo tokenInfo = kakaoService.processKakaoLogin(code);
             return ResponseEntity.ok(tokenInfo);
         } catch (HttpClientErrorException e) {
-            log.error("Kakao API error: {}", e.getResponseBodyAsString());
+            System.out.println("Kakao API error: {}" + e.getResponseBodyAsString());
             throw new RuntimeException("카카오 로그인 처리 중 오류가 발생했습니다: " + e.getStatusCode());
         } catch (Exception e) {
-            log.error("Error in kakao callback: ", e);
+            System.out.println("Error in kakao callback: " + e);
             return ResponseEntity.badRequest().body(new UserResponse.tokenInfo("Bearer", "Error: " + e.getMessage(), "null"));
         }
     }
