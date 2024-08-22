c2a91d6906a4f04925c9321c955d1e0073bd2fa5
zwei1garden <dlwjddnjs081723@naver.com>
2024-08-22 11:49:50 +0900

    [FIX] : https 카카오 로그인을 위한 리다이렉트 수정

--- src/main/java/com/umc/cardify/service/KakaoService.java
+++ src/main/java/com/umc/cardify/service/KakaoService.java
@@ -57,7 +57,7 @@ public class KakaoService {
             MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
             params.add("grant_type", "authorization_code");
             params.add("client_id", clientId);
-            params.add("client_secret", clientSecret);
+            // params.add("client_secret", clientSecret);
             params.add("redirect_uri", redirectUri);
             params.add("code", code);
 
