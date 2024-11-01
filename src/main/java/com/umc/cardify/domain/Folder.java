eabe71a15cb12822a6d07fd7a1a81c32afb1d7c5
SooD <dlatnqls921@naver.com>
2024-11-01 23:05:18 +0900

    [Feature] #201 :  Folder 엔티티에 하위 폴더 필드 생성

--- src/main/java/com/umc/cardify/domain/Folder.java
+++ src/main/java/com/umc/cardify/domain/Folder.java
@@ -51,4 +51,12 @@ public class Folder extends BaseEntity {
     public int getNoteCount() {
         return notes != null ? notes.size() : 0;
     }
+
+    // 하위폴더 - Folder 엔티티 자체로 재귀적 관계 설정
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_folder_id")
+    private Folder parentFolder;
+
+    @OneToMany(mappedBy = "parentFolder", cascade = CascadeType.ALL)
+    private List<Folder> subFolders = new ArrayList<>();
 }
