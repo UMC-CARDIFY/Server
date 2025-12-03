ada1a162aad0e060b21d2c629715118d8c103ddc
leegaarden <dlwjddnjs081723@naver.com>
2025-12-03 20:47:31 +0900

    feat #270 : 쿼리 파라미터 전달 방식으로 수정

--- src/main/java/com/umc/cardify/controller/AuthController.java
+++ src/main/java/com/umc/cardify/controller/AuthController.java
@@ -3,16 +3,11 @@ package com.umc.cardify.controller;
 import com.umc.cardify.auth.jwt.JwtTokenProvider;
 import com.umc.cardify.domain.User;
 import com.umc.cardify.repository.UserRepository;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +23,6 @@ public class AuthController {
 
     private final UserRepository userRepository;
     private final JwtTokenProvider tokenProvider;
-
 
     @PostMapping("/refresh")
     public ResponseEntity<?> refresh(HttpServletRequest request) {
