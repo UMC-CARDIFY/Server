1adea805298ad71e1297bb59bbab92924e5c2036
leegaarden <dlwjddnjs081723@naver.com>
2025-12-06 23:25:19 +0900

    feat : 도메인 적용에 따른 CorsConfig 수정

--- src/main/java/com/umc/cardify/config/CorsConfig.java
+++ src/main/java/com/umc/cardify/config/CorsConfig.java
@@ -18,6 +18,8 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of(
                 "https://cardify.co.kr",
+                "https://www.cardify.co.kr",
+                "https://api.cardify.co.kr",
                 "http://localhost:5173",
                 "http://localhost:8080",
                 "http://3.37.20.234:8080"
